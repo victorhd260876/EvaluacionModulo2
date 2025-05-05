653c8573b798e8a92c2d620dd652748584891d18
victorhd260876 <victor.huamandiaz@gmail.com>
2025-05-05 12:27:15 -0500

    implementacion de interfaces y sus metodos

---
+++ PlayaEstacionamiento/Repositories/iRepository.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PlayaEstacionamiento.Respositories
+{
+    //interfas generica q funciona con cualquier clase
+    public interface IRepository
+    {
+        List<T> GetAll(); //metodos genericos q se pueden utilizar en cualquier clase
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
